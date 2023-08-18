131f1001829659b63c00853057885fbda93e1db9
Ramazan Kubalov <k700pp@ya.ru>
2023-08-18 17:52:54 +0300

    Выполнил задание Урока №2

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+// ДЗ. Часть 1
+const title = 'KUBAL.RU';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 600;
+const rollback = 10;
+const fullPrice = 6900;
+const adaptive = true;
 
-alert('Hello');
-console.log('Hello again');
+// ДЗ. Часть 2
+console.log( typeof(title) );
+console.log( typeof(fullPrice) );
+console.log( typeof(adaptive) );
+
+console.log(screens.length);
+
+console.log(`Стоимость верстки экранов ${screenPrice} долларов`);
+console.log(`Стоимость разработки сайта ${fullPrice} долларов`);
+
+console.log( screens.toLocaleLowerCase().split(', ') );
+
+console.log( fullPrice * (rollback / 100) );
+
+// ДЗ. Часть 3
+
+
+
+
+
+
+
