6ed7f0e2dc1d03d9e93ea96f8eee221bb2f8d465
Ramazan Kubalov <k700pp@ya.ru>
2023-08-23 09:13:31 +0300

    Выполнил домашнее задание №3

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// ДЗ. Основная часть
-
-const title = 'KUBAL.RU';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 600;
-const rollback = 10;
-const fullPrice = 6900;
-const adaptive = true;
+let title = 'KUBAL.RU';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 1000;
+let fullPrice = 6900;
+let adaptive = true;
+let rollback = 0;
 
 console.log( typeof(title) );
 console.log( typeof(fullPrice) );
@@ -15,6 +13,38 @@ console.log(`Стоимость верстки экранов ${screenPrice} д�
 console.log(`Стоимость разработки сайта ${fullPrice} долларов`);
 console.log( screens.toLocaleLowerCase().split(', ') );
 console.log( fullPrice * (rollback / 100) );
+
+title = prompt('Как называется ваш проект');
+screens  = prompt('Какие типы экранов нужно разработать?');
+screenPrice  = +prompt('Сколько будет стоить данная работа?');
+adaptive  = confirm('Нужен ли адаптив на сайте?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let serviceCost1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let serviceCost2 = +prompt('Сколько это будет стоить?');
+
+fullPrice = screenPrice + serviceCost1 + serviceCost2;
+
+if (fullPrice > 30000) {
+    rollback = 10;
+    console.log('Попал в 1');
+}
+else if ((15000 < fullPrice) && (fullPrice <= 30000)) {
+    rollback = 5;
+    console.log('Попал в 2');
+}
+else if ((0 <= fullPrice) && (fullPrice <= 15000)) {
+    console.log('Скидка не предусмотрена');
+}
+else {
+    console.log('Что-то пошло не так');
+}
+
+let servicePercentPrice = fullPrice - Math.ceil(fullPrice * (rollback / 100) )
+console.log(`Итоговая стоимость за вычетом отката: ${servicePercentPrice}`);
+
+
+
 
 
 
